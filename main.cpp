27776c24aba4f9d1d63f0e42843a6f29953e41cb
Rodrigo <rodrigo.reyes@alumnos.ucn.cl>
2026-04-09 14:21:31 -0400

    Main commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
 
     const auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";

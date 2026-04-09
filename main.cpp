da0ab6390d5a70043a76e6b557ab0f7c2d795f3c
Rodrigo <rodrigo.reyes@alumnos.ucn.cl>
2026-04-09 14:22:32 -0400

    Main commit

--- main.cpp
+++ main.cpp
@@ -13,5 +13,4 @@ int main() {
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }

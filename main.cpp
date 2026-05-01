1e9ba84ea717491e64fa5105a3254bf6a2f7b8fa
Rodrigo <rodrigo.reyes@alumnos.ucn.cl>
2026-05-01 03:37:41 -0400

    Inicio de creación de menú de opciones, se argumentaron algunas funciones y instancias innecesarias debido a que no hemos trabajo con cfg todavía.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void lecturaCanciones(Almacenamiento* a) {
     string linea;
 
     if (!archivo.is_open()) {
-        cout << "Error: No se encontro music_source.txt" << endl;
+        cout << "No se encontro music_source.txt" << endl;
         return;
     }
 
@@ -36,10 +36,80 @@ void lecturaCanciones(Almacenamiento* a) {
     }
     archivo.close();
 }
+//LOS ARGUMENTOS DE ESTA FUNCIÓN SON TEMPORALES, lo hice asi porque una no he terminado la parte de la lista de reproducción,
+//Hay que crear una instancia de lista de reproducción y asigarle el estado actual del reproductor, y en base a esa instancia
+//sacar los argumentos de la función
+void menuOpciones(string estado, string modo, string cancion, string artista, string album, int anio) {
 
+    cout << estado << " (" << modo << "): " << cancion << endl;
+    cout << "Artista: " << artista << endl;
+    cout << "Album: " << album << " [" << anio << "]" << endl;
+    cout << "Opciones:" << endl;
+    cout << "  W - Reproducir/Pausar" << endl;
+    cout << "  Q - Pista Anterior" << endl;
+    cout << "  E - Pista Siguiente" << endl;
+    cout << "  S - Activar/Desactivar modo aleatorio" << endl;
+    cout << "  R - Repeticion (Desactivado/Repetir una/Repetir todas)" << endl;
+    cout << "  A - Ver lista de reproduccion actual" << endl;
+    cout << "  L - Listado de canciones" << endl;
+    cout << "  X - Salir" << endl;
+    cout << "Ingrese Opcion: ";
+}
 int main() {
     Almacenamiento* a = new Almacenamiento();
     lecturaCanciones(a);
     a->mostrarListaCanciones();
+    char opcion;
+    bool salir = false;
+
+    // Por mientras no tenemos cfg
+    string estadoActual = "Reproduciendo";
+    string modoActual = "S-R1";
+    string cancionActual = "Fire Dance";
+    string artistaActual = "Vivid BAD Squad";
+    string albumActual = "Fire Dance (Single)";
+    int anioActual = 2025;
+
+    while (!salir) {
+        menuOpciones(estadoActual, modoActual, cancionActual, artistaActual, albumActual, anioActual);
+        cin >> opcion;
+        //Pasa el input a mayusculas porsiaca
+        opcion = toupper(opcion);
+
+        switch (opcion) {
+            case 'W':
+                // pausa/play
+                break;
+            case 'Q':
+                // anterior
+                break;
+            case 'E':
+                // siguiente
+                break;
+            case 'S':
+                // aleatorio
+                break;
+            case 'R':
+                // ciclo de repetición
+                break;
+            case 'A':
+                // lista actual
+                break;
+            case 'L':
+                // catálogo completo
+                break;
+            case 'X':
+                //LOGICA PARA GUARDAR ESTADOOOO
+                salir = true;
+                break;
+            default:
+                //HAY QUE HACER LA LOGICA POR SI SE CAE EL SISTEMA
+                cout << "Opcion no valida. Intente de nuevo." << endl;
+                break;
+        }
+    }
+
+    delete a;
+    return 0;
     return 0;
 }

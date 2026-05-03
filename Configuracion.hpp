9be1841245c5c6c949eb60ce329362584f1c3d2f
josefa-v <josefa.vargas01@alumnos.ucn.cl>
2026-05-02 22:47:26 -0400

    commit josefa vargas

--- Configuracion.hpp
+++ Configuracion.hpp
@@ -4,12 +4,21 @@
 
 #ifndef TALLER1_ESTRUCTURA_CONFIGURACION_HPP
 #define TALLER1_ESTRUCTURA_CONFIGURACION_HPP
-
 #endif //TALLER1_ESTRUCTURA_CONFIGURACION_HPP
+
+struct nodoID {
+    int id;
+    nodoId* sig;
+};
+
+
 class Configuracion {
 private:
     int idCancionActual;
     bool estaPausado;
+    bool modoAleatorio;
+    int repeticion;
+    nodoID* listaPendientes;
 
 
 };
